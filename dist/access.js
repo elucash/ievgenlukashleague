19139e1ed3a62215f923bc500bc59019469ce82c
Eugene Lukash <a-eulukash@hotwire.com>
2016-12-04 12:03:33 -0800

    Switching to commit babel processed js

---
+++ dist/access.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// I'm using this pattern only for simple access
+// to the initialized objects of app and db
+// access to unitialized properties will throw an error
+var db = void 0,
+    app = void 0;
+
+exports.default = {
+  get db() {
+    if (!db) throw new Error("'db' is not initialized yet");
+    return db;
+  },
+  set db(v) {
+    db = v;
+  },
+  get app() {
+    if (!app) throw new Error("'app' is not initialized yet");
+    return app;
+  },
+  set app(v) {
+    app = v;
+  }
+};
---
+++ dist/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+var _mongodb = require("mongodb");
+
+var _mongodb2 = _interopRequireDefault(_mongodb);
+
+var _bodyParser = require("body-parser");
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _access = require("./access");
+
+var _access2 = _interopRequireDefault(_access);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// For the sake of simplicity, we'll try to connect to the database
+// and only when connection will succeed, there will be call to init
+// callback. If connection or server start will fail, we're better to
+// exit process and defer any actions like trying to restart us again
+// to the surrounding infrastructure.
+// The another aspect is storing 'app' and 'db' references
+
+function initApp() {
+  var app = (0, _express2.default)();
+  _access2.default.app = app;
+
+  app.use(_bodyParser2.default.json());
+
+  // registering routes on the app,
+  // we doing this lazily to make sure any modules referenced
+  // from route.js will be able to observe 'access {app, db}' already initialized
+  // having to use ["default"] is es6/babel quirk
+  require("./route")["default"](app);
+
+  var server = app.listen(process.env.PORT || 8080, function () {
+    console.log("running app on port: " + server.address().port);
+  });
+}
+
+function initDatabase() {
+  var url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+  _mongodb2.default.MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.error("Cannot connect to mongodb using url: " + url);
+      process.exit(1);
+    }
+    _access2.default.db = db;
+    console.log("Connection to mongodb established: " + url);
+
+    initApp();
+  });
+}
+
+initDatabase();
